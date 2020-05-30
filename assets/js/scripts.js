351a551220562bd598ab43a70d63da78705b45f4
arisrishana <66044824+arisrishana@users.noreply.github.com>
2020-05-30 07:07:53 +0700

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,59 @@
+$(window).on('load', function () {
+    $('#preloader-active').delay(450).fadeOut('slow');
+    $('body').delay(450).css({
+        'overflow': 'visible'
+    });
+});
+
+// Back to top button
+$(window).scroll(function() {
+if ($(this).scrollTop() > 100) {
+  $('.back-to-top').fadeIn('slow');
+} else {
+  $('.back-to-top').fadeOut('slow');
+}
+});
+$('.back-to-top').click(function(){
+$('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+return false;
+});
+    
+$(document).ready(()=>{
+    new WOW().init();
+
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+                this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length
+                ? target
+                : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate(
+                    {
+                        scrollTop: target.offset().top - 72,
+                    },
+                    1000,
+                    "easeInOutExpo"
+                );
+                return false;
+            }
+        }
+    });
+
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+});
