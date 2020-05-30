62809d780333d8cd20120e956b95f96fb2e92089
arisrishana <66044824+arisrishana@users.noreply.github.com>
2020-05-30 15:48:26 +0700

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,22 +7,16 @@ $(window).on('load', function () {
 
 // Back to top button
 $(window).scroll(function() {
-if ($(this).scrollTop() > 100) {
-  $('.back-to-top').fadeIn('slow');
-} else {
-  $('.back-to-top').fadeOut('slow');
-}
+  if ($(this).scrollTop() > 100) {
+    $('.back-to-top').fadeIn('slow');
+  } else {
+    $('.back-to-top').fadeOut('slow');
+  }
 });
 $('.back-to-top').click(function(){
   $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
   return false;
 });
-
-$('.go-to-contact').click(function(){
-  $('#contact').animate({scrollTop : 0},1500, 'easeInOutExpo');
-  return false;
-});
-
 
 $(document).ready(()=>{
     new WOW().init();
